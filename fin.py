6e93190d78ecfd5f5e77390733a9f9f2df74300a
AlexandrVino <64733647+ded-masay@users.noreply.github.com>
2021-04-13 17:33:48 +0500

    base_file

---
+++ fin.py
@@ -0,0 +1,160 @@
+import pygame
+import requests
+import sys
+import os
+
+import math
+
+# Подобранные константы для поведения карты.
+LAT_STEP = 0.008  # Шаги при движении карты по широте и долготе
+LON_STEP = 0.02
+coord_to_geo_x = 0.0000428  # Пропорции пиксельных и географических координат.
+coord_to_geo_y = 0.0000428
+
+
+# Определяем функцию, считающую расстояние между двумя точками, заданными координатами
+def lonlat_distance(a, b):
+    degree_to_meters_factor = 111 * 1000  # 111 километров в метрах
+    a_lon, a_lat = a
+    b_lon, b_lat = b
+
+    # Берем среднюю по широте точку и считаем коэффициент для нее.
+    radians_lattitude = math.radians((a_lat + b_lat) / 2.)
+    lat_lon_factor = math.cos(radians_lattitude)
+
+    # Вычисляем смещения в метрах по вертикали и горизонтали.
+    dx = abs(a_lon - b_lon) * degree_to_meters_factor * lat_lon_factor
+    dy = abs(a_lat - b_lat) * degree_to_meters_factor
+
+    # Вычисляем расстояние между точками.
+    distance = math.sqrt(dx * dx + dy * dy)
+
+    return distance
+
+# Найти объект по координатам.
+def reverse_geocode(ll):
+    geocoder_request_template = "http://geocode-maps.yandex.ru/1.x/?apikey=40d1649f-0493-4b70-98ba-98533de7710b&geocode={ll}&format=json"
+
+    # Выполняем запрос к геокодеру, анализируем ответ.
+    geocoder_request = geocoder_request_template.format(**locals())
+    response = requests.get(geocoder_request)
+
+    if not response:
+        raise RuntimeError(
+            """Ошибка выполнения запроса:
+            {request}
+            Http статус: {status} ({reason})""".format(
+                request=geocoder_request, status=response.status_code, reason=response.reason))
+
+    # Преобразуем ответ в json-объект
+    json_response = response.json()
+
+    # Получаем первый топоним из ответа геокодера.
+    features = json_response["response"]["GeoObjectCollection"]["featureMember"]
+    return features[0]["GeoObject"] if features else None
+
+# Структура для хранения результатов поиска:
+# координаты объекта, его название и почтовый индекс, если есть.
+
+class SearchResult(object):
+    def __init__(self, point, address, postal_code=None):
+        self.point = point
+        self.address = address
+        self.postal_code = postal_code
+
+# Параметры отображения карты:
+# координаты, масштаб, найденные объекты и т.д.
+
+class MapParams(object):
+    # Параметры по умолчанию.
+    def __init__(self):
+        self.lat = 55.729738  # Координаты центра карты на старте.
+        self.lon = 37.664777
+        self.zoom = 16  # Масштаб карты на старте.
+        self.type = "map"  # Тип карты на старте.
+
+        self.search_result = None  # Найденный объект для отображения на карте.
+        self.use_postal_code = False
+
+    # Преобразование координат в параметр ll
+    def ll(self):
+        return "{0},{1}".format(self.lon, self.lat)
+
+    # Обновление параметров карты по нажатой клавише.
+    def update(self, event):
+        if event.key == pygame.K_PAGEUP and self.zoom < 19:  # PG_UP
+            self.zoom += 1
+        elif event.key == pygame.K_PAGEDOWN and self.zoom > 2:  # PG_DOWN
+            self.zoom -= 1
+        elif event.key == pygame.K_LEFT:  # LEFT_ARROW
+            self.lon -= LON_STEP * math.pow(2, 15 - self.zoom)
+        elif event.key == pygame.K_RIGHT:  # RIGHT_ARROW
+            self.lon += LON_STEP * math.pow(2, 15 - self.zoom)
+        elif event.key == pygame.K_UP and self.lat < 85:  # UP_ARROW
+            self.lat += LAT_STEP * math.pow(2, 15 - self.zoom)
+        elif event.key == pygame.K_DOWN and self.lat > -85:  # DOWN_ARROW
+            self.lat -= LAT_STEP * math.pow(2, 15 - self.zoom)
+
+
+
+
+# Создание карты с соответствующими параметрами.
+def load_map(mp):
+    map_request = "http://static-maps.yandex.ru/1.x/?ll={ll}&z={z}&l={type}".format(ll=mp.ll(),
+                                                                                    z=mp.zoom,
+                                                                                    type=mp.type)
+    if mp.search_result:
+        map_request += "&pt={0},{1},pm2grm".format(mp.search_result.point[0],
+                                                   mp.search_result.point[1])
+
+    response = requests.get(map_request)
+    if not response:
+        print("Ошибка выполнения запроса:")
+        print(map_request)
+        print("Http статус:", response.status_code, "(", response.reason, ")")
+        sys.exit(1)
+
+    # Запишем полученное изображение в файл.
+    map_file = "map.png"
+    try:
+        with open(map_file, "wb") as file:
+            file.write(response.content)
+    except IOError as ex:
+        print("Ошибка записи временного файла:", ex)
+        sys.exit(2)
+
+    return map_file
+
+def main():
+    # Инициализируем pygame
+    pygame.init()
+    screen = pygame.display.set_mode((600, 450))
+
+    # Заводим объект, в котором будем хранить все параметры отрисовки карты.
+    mp = MapParams()
+
+    while True:
+        event = pygame.event.wait()
+        if event.type == pygame.QUIT:  # Выход из программы
+            break
+        elif event.type == pygame.KEYDOWN:  # Обрабатываем различные нажатые клавиши.
+            mp.update(event)
+        else:
+            continue
+
+        # Загружаем карту, используя текущие параметры.
+        map_file = load_map(mp)
+
+        # Рисуем картинку, загружаемую из только что созданного файла.
+        screen.blit(pygame.image.load(map_file), (0, 0))
+
+        # Переключаем экран и ждем закрытия окна.
+        pygame.display.flip()
+
+    pygame.quit()
+    # Удаляем за собой файл с изображением.
+    os.remove(map_file)
+
+
+if __name__ == "__main__":
+    main()
